35572b34eeb08020804f68a7b456e51cfd909d96
nanazzoo <101784680+nanazzoo@users.noreply.github.com>
2022-04-15 16:55:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const clock = document.getElementById("clock");
+
+function getTime() {
+  const time = new Date();
+  const hour = String(time.getHours()).padStart(2, "0");
+  const minutes = String(time.getMinutes()).padStart(2, "0");
+  const seconds = String(time.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hour}:${minutes}:${seconds}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
